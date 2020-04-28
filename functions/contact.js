f6799142d27a0ae1c61fdc463f3eedf52ed6c87e
Conor Campbell <conorc1000@gmail.com>
2020-04-28 11:10:47 +1000

    emails setup with mailgun

--- functions/contact.js
+++ functions/contact.js
@@ -1,8 +1,32 @@
-exports.handler = (event, _context, callback) => {
-    console.log({event});
+require('dotenv').config();
 
-    callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({boop: true})
+exports.handler = (event, _context, callback) => {
+
+    const mailgun = require('mailgun-js');
+    console.log("process.env.MAILGUN_API_KEY", process.env.MAILGUN_API_KEY)
+
+    const mg = mailgun({
+        apiKey: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN,
+
+    })
+
+
+    const data = JSON.parse(event.body);
+
+    console.log("data", data)
+
+    const email = {
+        from: `Conor Campbell <test@gmail.com>`,
+        to: `${data.email}`,
+        subject: data.subject,
+        text: data.body
+    }
+
+    mg.messages().send(email, (error, response) => {
+        callback(error, {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        })
     })
 }
